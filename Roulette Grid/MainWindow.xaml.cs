90010344596925b99a86368ac12499ec04798cda
Dion <dulellari.dion@gmail.com>
2023-04-14 12:30:45 +0300

    Add project files.

---
+++ Roulette Grid/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Roulette_Grid
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            EventManager.RegisterClassHandler(typeof(Button), Button.ClickEvent, new RoutedEventHandler(Button_Click));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            var txt = ((Button)sender).Content ?? (((Button)sender).Background.ToString() == "#FFFF0000" ? "Red" : "Black");
+
+            MessageBox.Show(txt.ToString());
+           
+        }
+    }
+
+}
